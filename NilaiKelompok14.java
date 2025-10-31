0939112110c1689411bc8c65ed110f4d7b45b2ce
ki1rei <studyacctyara@gmail.com>
2025-10-31 10:39:43 +0700

    (~) : Nilai tertinggi kelompok dan rata - rata

--- NilaiKelompok14.java
+++ NilaiKelompok14.java
@@ -6,6 +6,8 @@ public class NilaiKelompok14 {
 
         int i = 1, j, nilai;
         float totalNilai, rataNilai;
+        float rataTertinggi = 0;
+        int kelompokTertinggi = 0;
 
         while (i <= 6) {
             System.out.println("Kelompok " + i);
@@ -16,12 +18,18 @@ public class NilaiKelompok14 {
                 nilai = sc.nextInt();
                 totalNilai += nilai;
             }
-
             rataNilai = totalNilai / 5;
             System.out.println("Kelompok " + i + ": nilai rata-rata = " + rataNilai);
             System.out.println();
 
+            if (rataNilai > rataTertinggi) {
+                rataTertinggi = rataNilai;
+                kelompokTertinggi = i;
+            }
+
             i++;
         }
+        System.out.println("Kelompok dengan rata-rata tertinggi adalah Kelompok " 
+                           + kelompokTertinggi + " dengan nilai " + rataTertinggi);
     }
 }
